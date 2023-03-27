bfda99bc6179fef3c94ba472f6e7f16c9e71bf9d
NojusMat <d00249674@student.dkit.ie>
2023-03-27 13:07:19 +0100

    Bug.cpp created

--- Bug.cpp
+++ Bug.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "Bug.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+Bug::Bug(int id,int direction,int size,bool alive) {
+    this->id = id;
+    this->direction = direction;
+    this->size = size;
+    this->alive = alive;
+
+}
