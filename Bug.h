19d688c4da72e853faf076122b2c2f5d77b0cd80
NojusMat <d00249674@student.dkit.ie>
2023-03-27 14:51:59 +0100

    Bug.h getters and setters

--- Bug.h
+++ Bug.h
@@ -11,17 +11,34 @@ enum class Direction{North,East,South,West};
 
 class Bug{
     public:
-    Bug(int id,std::pair<int, int> position,Direction direction,int size);
+    Bug(int id,std::pair<int, int> position,Direction direction,int size); //Constructor
 
-    virtual ~Bug() =default;       // DESTRUCTOR
     virtual void move()=0;
+    virtual ~Bug() =default;       // DESTRUCTOR
 
-    int getid();
-    std::pair<int, int> getposition();
-    Direction getdirection();
-    int getsize();
-    bool getalive();
-    bool isWayBlocked();
+
+    int getid()const;                            // getting info for bug
+    std::pair<int, int> getPosition()const;
+    Direction getDirection()const;
+    int getSize()const;
+    bool getAlive()const;
+    bool isWayBlocked()const;
+
+    void setPosition(const std::pair<int, int> getposition);   //setting info for bug
+    void setDirection(Direction direction);
+    void setSize(int size);
+    void setAlive(bool alive);
+    void setPath( const std::list<std::pair<int,int>> pathconst);
+
+
+    private:
+    int id;
+    std::pair<int, int> position;
+    Direction direction;
+    int size;
+    bool alive;
+    std::list<std::pair<int,int>> path;
+
 };
 
 
