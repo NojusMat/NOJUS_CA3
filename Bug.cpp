f0175972e23eb9b08d7e5af44535f935c3dddb45
NojusMat <d00249674@student.dkit.ie>
2023-03-27 12:51:22 +0100

    Testing Commit / Created Bug class

---
+++ Bug.cpp
@@ -0,0 +1,5 @@
+//
+// Created by wwwno on 27/03/2023.
+//
+
+#include "Bug.h"
---
+++ Bug.h
@@ -0,0 +1,14 @@
+//
+// Created by wwwno on 27/03/2023.
+//
+
+#ifndef NOJUS_CA3_BUG_H
+#define NOJUS_CA3_BUG_H
+
+
+class Bug {
+
+};
+
+
+#endif //NOJUS_CA3_BUG_H
