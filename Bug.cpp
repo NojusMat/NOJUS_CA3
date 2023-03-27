d45975128397b6e763c35ef9660adb0f4f33babf
NojusMat <d00249674@student.dkit.ie>
2023-03-27 15:51:46 +0100

    added set/get alive to bug.cpp

--- Bug.cpp
+++ Bug.cpp
@@ -25,9 +25,9 @@ void Bug::setID(int id_bug){
 }
 
 
-//std::pair<int, int> Bug::getPosition() const {   ASK HOW TO DO PAIRS FOR GETTERS
-//    return position();
-//}
+std::pair<int, int> Bug::getPosition() const {
+    return position;
+}
 
 void Bug::setPosition(const std::pair<int, int> position_bug) {
     position=position_bug;
@@ -52,8 +52,13 @@ void Bug::setSize(int size_bug) {
 }
 
 
-//Bug::~Bug(){
-//}
+bool Bug::getAlive() const {
+    return alive;
+}
+
+void Bug::setAlive(bool alive_bug) {
+    alive=alive_bug;
+}
 
 
 
